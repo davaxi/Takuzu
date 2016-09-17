fc46ed8ca6aab5cddf3f9dd255851a055f90402d
David Patiashvili <stratagem.david@gmail.com>
2016-09-17 21:20:39 +0200

    ResolverSeriesGenerator: Simplify methods

--- src/ResolverSeriesGenerator.php
+++ src/ResolverSeriesGenerator.php
@@ -8,105 +8,125 @@ namespace Davaxi\Takuzu;
  */
 class ResolverSeriesGenerator
 {
+    protected static $values = array(
+        Grid::ZERO,
+        Grid::ONE
+    );
+
     /**
      * @param array $needs
-     * @param array $leftRangeValues
-     * @param array $rightRangeValues
+     * @param array $leftValues
+     * @param array $rightValues
      * @return array
      */
-    public static function getRangePossibilities(array $needs, $leftRangeValues = array(), $rightRangeValues = array())
+    public static function getRangePossibilities(array $needs, $leftValues = array(), $rightValues = array())
     {
         $rangeLength = array_sum($needs);
         if (!$rangeLength) {
             return array();
         }
 
-        $lengthPossibilities = array();
-        $rightRangeCount = count($rightRangeValues);
-        $leftRangeCount = count($leftRangeValues);
-        if ($leftRangeCount > 0) {
-            $lengthPossibilities[] = array(
-                'values' => $leftRangeValues,
-                'needs' => $needs,
-            );
-            $rangeLength += $leftRangeCount;
-            $currentLength = $leftRangeCount;
-        }
-        else {
-            $currentLength = 1;
-            if ($needs[Grid::ZERO]) {
-                $availableValue = $needs;
-                $availableValue[Grid::ZERO]--;
-
-                $lengthPossibilities[] = array(
-                    'values' => array(Grid::ZERO),
-                    'needs' => $availableValue,
+        $possibility = array(
+            'needs' => $needs,
+            'needsCount' => array_sum($needs),
+            'values' => $leftValues,
+        );
+        $possibilities = static::getPossibilities($possibility);
+        while ($possibilities && $possibilities[0]['needsCount']) {
+            $newPossibilities = array();
+            foreach ($possibilities as $possibility) {
+                $newPossibilities = array_merge(
+                    $newPossibilities,
+                    static::getPossibilities($possibility)
                 );
             }
-            if ($needs[Grid::ONE]) {
-                $availableValue = $needs;
-                $availableValue[Grid::ONE]--;
-                $lengthPossibilities[] = array(
-                    'values' => array(Grid::ONE),
-                    'needs' => $availableValue,
+            $possibilities = $newPossibilities;
+
+        }
+        $rightValues = array_slice($rightValues, 0, 2);
+        foreach ($rightValues as $rightValue) {
+            $newPossibilities = array();
+            foreach ($possibilities as $possibility) {
+                $possibility['needs'][$rightValue] = 1;
+                $newPossibilities = array_merge(
+                    $newPossibilities,
+                    static::getPossibilities($possibility)
                 );
             }
+            $possibilities = $newPossibilities;
         }
-        do {
-            if (!$lengthPossibilities) {
-                break;
-            }
-            $nextLengthPossibilities = array();
-            foreach ($lengthPossibilities as $lengthPossibility) {
-                $zeroAvailable = true;
-                $oneAvailable = true;
-                if ($currentLength > 1) {
-                    $previousValue1 = $lengthPossibility['values'][$currentLength - 1];
-                    $previousValue2 = $lengthPossibility['values'][$currentLength - 2];
-                    if ($previousValue1 === $previousValue2) {
-                        $zeroAvailable = $previousValue1 == Grid::ONE;
-                        $oneAvailable = $previousValue1 == Grid::ZERO;
-                    }
-                }
-                if ($zeroAvailable && $lengthPossibility['needs'][Grid::ZERO]) {
-                    $newLengthPossibility = $lengthPossibility;
-                    $newLengthPossibility['needs'][Grid::ZERO]--;
-                    $newLengthPossibility['values'][] = Grid::ZERO;
-                    $nextLengthPossibilities[] = $newLengthPossibility;
-                }
+        return array_map(
+            function(array $possibility) use ($leftValues, $rangeLength) {
+                return array_slice(
+                    $possibility['values'],
+                    count($leftValues),
+                    $rangeLength
+                );
+            },
+            $possibilities
+        );
+    }
 
-                if ($oneAvailable && $lengthPossibility['needs'][Grid::ONE]) {
-                    $newLengthPossibility = $lengthPossibility;
-                    $newLengthPossibility['needs'][Grid::ONE]--;
-                    $newLengthPossibility['values'][] = Grid::ONE;
-                    $nextLengthPossibilities[] = $newLengthPossibility;
-                }
-            }
-            $currentLength++;
-            $lengthPossibilities = $nextLengthPossibilities;
-
-        } while($currentLength !== $rangeLength);
-
+    /**
+     * @param array $possibility
+     * @return array
+     */
+    protected static function getPossibilities(array $possibility)
+    {
         $possibilities = array();
-        foreach ($lengthPossibilities as $possibility) {
-            if ($rightRangeCount > 0 && $currentLength > 2) {
-                $previousValue1 = $possibility['values'][$currentLength - 1];
-                if ($previousValue1 === $rightRangeValues[0]) {
-                    $previousValue2 = $possibility['values'][$currentLength - 2];
-                    if ($previousValue1 === $previousValue2) {
-                        continue;
-                    }
-                    if ($rightRangeCount > 1 && $rightRangeValues[0] === $rightRangeValues[1]) {
-                        continue;
-                    }
-                }
+        foreach (static::$values as $value) {
+            if (!$possibility['needs'][$value]) {
+                continue;
             }
-            if ($leftRangeValues) {
-                $possibility['values'] = array_slice($possibility['values'], $leftRangeCount);
+            if (!static::checkRange($possibility['values'], $value)) {
+                continue;
             }
-            $possibilities[] = array_values($possibility['values']);
+            $possibilities[] = static::getPossibility(
+                $possibility['values'],
+                $possibility['needs'],
+                $value
+            );
         }
         return $possibilities;
+    }
+
+    /**
+     * @param array $values
+     * @param array $needs
+     * @param $useValue
+     * @return array
+     */
+    protected static function getPossibility(array $values, array $needs, $useValue)
+    {
+        if ($needs[$useValue] <= 0) {
+            throw new \LogicException('Invalid call to method');
+        }
+        $needs[$useValue]--;
+        $values[] = $useValue;
+        return array(
+            'values' => $values,
+            'needs' => $needs,
+            'needsCount' => array_sum($needs),
+        );
+    }
+
+    /**
+     * @param $values
+     * @param $newValue
+     * @return bool
+     */
+    protected static function checkRange($values, $newValue)
+    {
+        $length = count($values);
+        if ($length > 1) {
+            $lastValue = array_pop($values);
+            $previousValue = array_pop($values);
+            if ($lastValue !== $previousValue) {
+                return true;
+            }
+            return $lastValue !== $newValue;
+        }
+        return true;
     }
 
 }
