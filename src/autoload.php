232b916a9a632c04b3f195193426889a4dd4c059
David Patiashvili <stratagem.david@gmail.com>
2016-09-21 20:58:55 +0200

    Autoload: Update method to require files

--- src/autoload.php
+++ src/autoload.php
@@ -5,13 +5,12 @@
 function davaxi_takuzu_autoload($className)
 {
     $classPath = explode('\\', $className);
-    if ($classPath[0] != 'Davaxi' || $classPath[1] != 'Takuzu') {
-        return;
-    }
-    $classPath = array_slice($classPath, 2);
-    $filePath = dirname(__FILE__) . '/' . implode('/', $classPath) . '.php';
-    if (file_exists($filePath)) {
-        require_once($filePath);
+    if ($classPath[0] === 'Davaxi' && $classPath[1] === 'Takuzu') {
+        $classPath = array_slice($classPath, 2);
+        $filePath = dirname(__FILE__) . '/' . implode('/', $classPath) . '.php';
+        if (file_exists($filePath)) {
+            require_once($filePath);
+        }
     }
 }
 spl_autoload_register('davaxi_takuzu_autoload');
